bb07a993ba7ca0cdf34bbec6a4b0738c266ec987
MariaDul Combs <dulce@MariaDul.local>
2021-12-17 20:04:58 -0600

    JS Course upload

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,34 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('JS is FUN')
+
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10)
+
+// VALUE: a piece of data
+// smallest unit of info we have in JS
+
+// Conventions ans rules to name variables:
+// camel case: firstNamePerson
+
 let js = 'amazing';
-if (js === 'amazing') alert('JS is FUN')
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10)
+
+console.log('Dulce');
+console.log(43);
+
+let firstName = 'Lolis';
+let first = 'Dul';
+let firstNamePerson;
+//let first_name_person; //more common in Ruby
+
+console.log(firstName); // line 24
+
+// ERRORS
+//let 3years = 3; // error
+//let dul&mary = "DM"; // error
+//let new = 27;
+let $function = 27; // accepted
+
+
+
+
